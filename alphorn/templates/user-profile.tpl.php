242e39fbac41761ad2f99ad7a156d8dfacb5c5fc
koencolaers <koen.colaers@cegeka.be>
2012-09-28 13:36:14 +0200

    Moved remove to css

--- alphorn/templates/user-profile.tpl.php
+++ alphorn/templates/user-profile.tpl.php
@@ -1,6 +1,6 @@
 <div id="user-profile">
     <h1>
-        <?php print $name; ?>
+        <?php <drupal_set_title(''); print $name; ?>
     </h1>
     <table id="user-profile-info">
         <tr><td>Member since:</td><td><?php print $street;?></td><td><?php print $date_of_birth;?></td></tr>
